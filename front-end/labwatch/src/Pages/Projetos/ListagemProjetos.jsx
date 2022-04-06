4acf6b7f5eada9ebacedc327ea3106da5880bbfe
Lucas <lucask.silvamendes@gmail.com>
2022-04-06 16:57:10 -0300

    alteracao alerta

--- front-end/labwatch/src/Pages/Projetos/ListagemProjetos.jsx
+++ front-end/labwatch/src/Pages/Projetos/ListagemProjetos.jsx
@@ -402,7 +402,7 @@ export default function ListagemProjetos() {
                                                                 className="btn__excluirProjeto btn"
                                                                 onClick={() => excluirProjeto()}>Sim</button>
                                                             <button
-                                                                className="btn__backProjeto btn"
+                                                                className="btn__Excluir"
                                                                 onClick={() => fecharAlerta()}
                                                             >Não</button>
                                                         </div>
@@ -573,7 +573,7 @@ export default function ListagemProjetos() {
                                                             className="btn__excluirProjeto btn"
                                                             onClick={() => excluirProjeto()}>Sim</button>
                                                         <button
-                                                            className="btn__backProjeto btn"
+                                                            className="btn__Excluir"
                                                             onClick={() => fecharAlerta()}
                                                         >Não</button>
                                                     </div>
