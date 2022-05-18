a81cbdab29ceed55d91a4c2b4afbd05f0e2eebba
Gustavo Lima <glima.0913@gmail.com>
2022-05-18 17:13:46 -0300

    Telas de gráfico iniciadas

--- src/Pages/Dashboard/Graficos.js
+++ src/Pages/Dashboard/Graficos.js
@@ -1,7 +1,7 @@
 import { React, useState, useEffect } from "react";
 import axios from "axios";
 // import { parseJwt } from "../../services/auth";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 
 import Header from '../../Components/header';
 import SideBar from '../../Components/sidebar'
@@ -15,43 +15,49 @@ import GraficoPizza from "./Components/pizza.jsx";
 
 import GraficoColuna from "./Components/Coluna.jsx";
 import GraficoLinhas from "./Components/Linhas.jsx";
+import QntdTask from "./Components/qntdTask.jsx";
 
 
 
-    function App() {
-        return (
-        <div>
-        <Header />
-          <SideBar />
-        <div>
-          <div className="App">
-            <div class="container">
+function App() {
+
+
+  return (
+    <div>
+      <Header />
+      <SideBar />
+      <div>
+        <div className="App">
+          <div class="container">
             <h1 className='Title' >DashBoard</h1>
-              <div class="row">
-                <div className="boxGraphs1">
-                  <BarChartChartUserRegisterbyMonth />
-                </div>
-                <div  className="boxGraphs2">
-                  <GraficoLinhas />
-                </div>
+            {/* <div class="row">
+              <div className="boxGraphs1">
+                <BarChartChartUserRegisterbyMonth />
               </div>
-              <div class="row">
-                <div className="boxGraphs3">
-                  <GraficoPizza />
-                </div>
-                <div  className="boxGraphs4">
-                  <GraficoColuna />
-                </div>
+              <div className="boxGraphs2">
+                <GraficoLinhas />
               </div>
             </div>
+            <div class="row">
+              <div className="boxGraphs3">
+                <GraficoPizza />
+              </div>
+              <div className="boxGraphs4">
+                <GraficoColuna /> */}
+            {/* </div> */}
+            {/* </div> */}
           </div>
-        </div> 
         </div>
-        );
-      }    
+      </div>
+      <div className="boxGraphs1">
+        <QntdTask />
+      </div>
+    </div>
+  );
+}
 
-    
 
-    export default App;
-    
+
+export default App;
+
 
